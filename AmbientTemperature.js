3755c679054c4972caa3e4fa843fac68d5594a18
Sumedha Verma <1967763+sumedhav@users.noreply.github.com>
2022-11-08 20:07:24 +0100

    Update AmbientTemperature plugin

--- AmbientTemperature.js
+++ AmbientTemperature.js
@@ -17,7 +17,7 @@ const TemperatureTile = (vssSignal, initialTemperature, finalTemperature, vehicl
         <div style="height: 100%; display: flex; flex-direction: column;">
             <div style="display: flex; flex-direction: column; height: 100%; background-color: rgb(103 156 146); color: white; padding: 35px 20px; user-select: none; align-items: center;" data-signal="${vssSignal}">
                 <div style="margin-bottom: 10px;font-size: 1.1em;font-weight: bold;margin-top: auto;overflow: hidden;text-overflow: ellipsis;width: fit-content; max-width: 100%;" title="${label}">${label}</div>
-                <div style="font-size: 1.1em; margin-bottom: 25px;" class="signal-value"><span></span> °C</div>
+                <div style="font-size: 1.1em; margin-bottom: 25px;" class="signal-value"><span>${initialTemperature}</span> °C</div>
                 <div style="margin-top: auto;"><i style="font-size: 3em;" class="fa-solid fa-temperature-half"></i></div>
             </div>
         </div>
@@ -27,16 +27,16 @@ const TemperatureTile = (vssSignal, initialTemperature, finalTemperature, vehicl
             const strippedApi = vssSignal.split(".").slice(1).join(".")
             const isAirConOn = await vehicle[strippedApi].get()
             const signalValueEl = div.querySelector(`[data-signal="${vssSignal}"] .signal-value > span`)
-            if(isAirConOn && signalValueEl !== null && currentTemperature > finalTemperature - 2) {
+            if(isAirConOn && signalValueEl !== null && currentTemperature > finalTemperature) {
                 signalValueEl.textContent = currentTemperature
-                currentTemperature -= 2
+                currentTemperature -= 1
             }
             if (!isAirConOn && signalValueEl !== null) {
                 signalValueEl.textContent = initialTemperature
             } else {
                 throw new Error(`div couldn't be found.`)
             }
-        }, 300)
+        }, 500)
 
         box.injectNode(div)
 
