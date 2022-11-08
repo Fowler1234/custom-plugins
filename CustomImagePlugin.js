031dfe9adad723ed03d232f8684ce2782ff47465
Fowler1234 <117686203+Fowler1234@users.noreply.github.com>
2022-11-08 13:26:24 +0100

    Update CustomImagePlugin.js

--- CustomImagePlugin.js
+++ CustomImagePlugin.js
@@ -1,10 +1,25 @@
 const plugin = ({widgets, simulator, vehicle}) => {
     const container = document.createElement("div")
     container.setAttribute("style", `height: 100%; width: 100%;`)
+    
+     let options = {
+      method: 'GET',      
+      headers: {}
+    };
+
+    fetch('https://raw.githubusercontent.com/Fowler1234/custom-plugins/main/CustomImagePlugin.js', options)
+    .then(response => response.text())
+    .then(body => {
+      console.log(body);
+    });
+    
     container.innerHTML = (`
-    <div style="max-width: fit-content; margin: 0 auto; position: relative;">
-        <div class="smartphone-text" style="position: absolute; color: red; font-family: 'Lato'; width: 100%; top: 0; height: 100%; box-sizing: border-box; padding-top: 25px; padding-right: 12px; padding-left: 12px; padding-bottom: 25px;"></div>
-    </div>
+        <script>console.log("Hello World!!!")</script>
+       <div style="max-width: fit-content; margin: 0 auto; position: relative;">
+	     <h1 class="smartphone-text" style="color: red; font-family: 'Lato'; width: 100%;height: 100%; box-sizing: border-box; padding-top: 25px; padding-right: 12px; padding-left: 12px; padding-bottom: 25px;">
+          Welcome, Carla!!
+         </h1>
+     </div>
     `)
 
     let boxGlobal = null
