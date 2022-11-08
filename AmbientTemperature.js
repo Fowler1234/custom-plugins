7c6d46092151c6751f38e4a70c531d0310c6a797
Sumedha Verma <1967763+sumedhav@users.noreply.github.com>
2022-11-08 18:20:15 +0100

    Update AmbientTemperature plugin

--- AmbientTemperature.js
+++ AmbientTemperature.js
@@ -16,19 +16,19 @@ const TemperatureTile = (isAirConOn, initialTemperature, finalTemperature) => {
         <div style="height: 100%; display: flex; flex-direction: column;">
             <div style="display: flex; flex-direction: column; height: 100%; background-color: rgb(31 41 55); color: white; padding: 35px 20px; user-select: none; align-items: center;" data-signal="${isAirConOn}">
                 <div style="margin-bottom: 10px;font-size: 1.1em;font-weight: bold;margin-top: auto;overflow: hidden;text-overflow: ellipsis;width: fit-content; max-width: 100%;" title="${label}">${label}</div>
-                <div style="font-size: 1.1em; margin-bottom: 25px;" class="signal-value"><span>${initialTemperature}</span> °C</div>
+                <div style="font-size: 1.1em; margin-bottom: 25px;" class="signal-value"><span></span> °C</div>
                 <div style="margin-top: auto;"><i style="font-size: 3em;" class="fa-solid fa-temperature-half"></i></div>
             </div>
         </div>
         `)
 
-        // const intervalId = setInterval(async () => {
-        //     const signalValueEl = div.querySelector(`[data-signal="${isAirConOn}"] .signal-value > span`)
-        //     if (signalValueEl !== null) {
-        //         signalValueEl.textContent = 30
-        //     } else {
-        //         throw new Error(`Signal Pill div couldn't be found.`)
-        // }, 300)
+        const intervalId = setInterval(async () => {
+            const signalValueEl = div.querySelector(`[data-signal="${isAirConOn}"] .signal-value > span`)
+            if (signalValueEl !== null) {
+                signalValueEl.textContent = initialTemperature
+            } else {
+                throw new Error(`div couldn't be found.`)
+        }, 300)
 
         box.injectNode(div)
 
